87e334e079c95fedc673b563fbff74e2b0fd2cda
Danil Zharkov <zharkov.danik.ua@gmail.com>
2026-03-13 21:16:37 +0000

    feat: connection to websocket

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,25 @@
 import express from 'express'
+import { configDotenv } from 'dotenv'
+import cors from 'cors'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+
+configDotenv()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(cors())
+app.use(express.json())
+
+const server = createServer(app)
+const io = new Server(server, { cors: { origin: '*' } })
+
+io.on('connection', () => {
+  console.log('user is connected')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
